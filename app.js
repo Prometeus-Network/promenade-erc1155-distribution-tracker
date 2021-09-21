020a73be9b43285ce381453874d1a645b136184a
Max <maxim.valenko@farfetch.com>
2021-09-21 12:51:07 +0300

    add healthcheck

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
+const healthcheck = require('./healthcheck');
 
 require('./models/erc1155contract')
 require('./models/nftitems')
@@ -14,5 +15,6 @@ const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', async () => {
   console.log('nifty server has been connected to the db server')
+  healthcheck();
   track1155Distribution()
 })
